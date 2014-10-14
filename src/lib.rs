996a9fe62ee8533d8d0259c974a142b8c28d546b
Kevin Ballard <kevin@sb.org>
2014-10-14 00:19:30 -0700

    Add helper type XMLWriter
    XMLWriter is used to manage the XML serialization of Items.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,45 @@
 //! Helpers for writing Alfred XML output
+//!
+//! # Example
+//!
+//! ```
+//! extern crate alfred;
+//!
+//! use std::io;
+//!
+//! fn write_items() -> io::IoResult<()> {
+//!     let mut xmlw = try!(alfred::XMLWriter::new(io::stdout()));
+//!
+//!     let item1 = alfred::Item::new("Item 1");
+//!     let item2 = alfred::Item {
+//!         subtitle: Some("Subtitle".into_maybe_owned()),
+//!         ..alfred::Item::new("Item 2")
+//!     };
+//!     let item3 = alfred::Item {
+//!         arg: Some("Argument".into_maybe_owned()),
+//!         subtitle: Some("Subtitle".into_maybe_owned()),
+//!         icon: Some(alfred::FileType("public.folder".into_maybe_owned())),
+//!         ..alfred::Item::new("Item 3")
+//!     };
+//!
+//!     try!(xmlw.write_item(&item1));
+//!     try!(xmlw.write_item(&item2));
+//!     try!(xmlw.write_item(&item3));
+//!
+//!     xmlw.close()
+//! }
+//!
+//! fn main() {
+//!     match write_items() {
+//!         Ok(()) => {},
+//!         Err(err) => {
+//!             let _ = writeln!(io::stderr(), "Error writing items: {}", err);
+//!         }
+//!     }
+//! }
+//! ```
 
+#![feature(if_let)]
 #![warn(missing_doc)]
 
 use std::io;
@@ -66,8 +106,82 @@ pub enum ItemType {
     FileItemType
 }
 
+/// Helper struct used to manage the XML serialization of `Item`s
+///
+/// When the `XMLWriter` is first created, the XML header is immediately
+/// written. When the `XMLWriter` is dropped, the XML footer is written.
+///
+/// Any errors produced by writing the footer are silently ignored. The
+/// `close()` method can be used to return any such error.
+pub struct XMLWriter<'a, W: Writer + 'a> {
+    w: W,
+    last_err: Option<io::IoError>
+}
+
+impl<'a, W: Writer + 'a> XMLWriter<'a, W> {
+    /// Returns a new `XMLWriter` that writes to the given `Writer`
+    ///
+    /// The XML header is written immediately.
+    pub fn new(mut w: W) -> io::IoResult<XMLWriter<'a, W>> {
+        match w.write_str("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<items>\n") {
+            Ok(()) => {
+                Ok(XMLWriter {
+                    w: w,
+                    last_err: None
+                })
+            }
+            Err(err) => Err(err)
+        }
+    }
+
+    /// Writes an `Item` to the underlying `Writer`
+    ///
+    /// If a previous write produced an error, any subsequent write will do
+    /// nothing and return the same error. This is because the previous write
+    /// may have partially completed, and attempting to write any more data
+    /// will be unlikely to work properly.
+    pub fn write_item(&mut self, item: &Item) -> io::IoResult<()> {
+        if let Some(ref err) = self.last_err {
+            return Err(err.clone());
+        }
+        let result = item.write_xml(&mut self.w, 1);
+        if let Err(ref err) = result {
+            self.last_err = Some(err.clone());
+        }
+        result
+    }
+
+    /// Consumes the `XMLWriter` and writes the XML footer
+    ///
+    /// This method can be used to get any error resulting from writing the
+    /// footer. If this method is not used, the footer will be written when the
+    /// `XMLWriter` is dropped and any error will be silently ignored.
+    ///
+    /// As with `write_item()`, if a previous invocation of `write_item()`
+    /// returned an error, `close()` will return the same error without
+    /// attempting to write the XML footer.
+    pub fn close(mut self) -> io::IoResult<()> {
+        if let Some(err) = self.last_err {
+            return Err(err);
+        }
+        self.w.write_str("</items>\n")
+    }
+}
+
+/// Writes a complete XML document representing the `Item`s to the `Writer`
+pub fn write_items<W: Writer>(w: W, items: &[Item]) -> io::IoResult<()> {
+    let mut xmlw = try!(XMLWriter::new(w));
+    for item in items.iter() {
+        try!(xmlw.write_item(item));
+    }
+    xmlw.close()
+}
+
 impl<'a> Item<'a> {
-    /// Writes the XML fragment representing the Item to the Writer
+    /// Writes the XML fragment representing the `Item` to the `Writer`
+    ///
+    /// `XMLWriter` should be used instead if at all possible, in order to
+    /// write the XML header/footer and maintain proper error discipline.
     pub fn write_xml(&self, w: &mut io::Writer, indent: uint) -> io::IoResult<()> {
         fn write_indent(w: &mut io::Writer, indent: uint) -> io::IoResult<()> {
             for _ in range(0, indent) {
